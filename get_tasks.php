d8bcc2070ec1ec25c04aab6e641319ddbc20093e
Mehdi-Elargoubi <mehdi.elargoubi11@gmail.com>
2024-11-23 11:54:39 +0100

    Modification d'affichage de update_at

--- get_tasks.php
+++ get_tasks.php
@@ -13,9 +13,15 @@ if ($conn->connect_error) {
 }
 
 // Récupérer les tâches en triant par l'état de la tâche (complétée ou non), et par updated_at ou created_at
-$sql = "SELECT id, title, description, completed, 
-        IFNULL(updated_at, created_at) AS updated_at, 
-        created_at 
+$sql = "SELECT id, 
+               title, 
+               description, 
+               completed, 
+               CASE 
+                   WHEN updated_at IS NULL THEN 'Non modifié' 
+                   ELSE DATE_FORMAT(updated_at, '%Y-%m-%d %H:%i:%s') 
+               END AS updated_at, 
+               DATE_FORMAT(created_at, '%Y-%m-%d %H:%i:%s') AS created_at 
         FROM tasks 
         ORDER BY completed ASC, updated_at DESC";
 
